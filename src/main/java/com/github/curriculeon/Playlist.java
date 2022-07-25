68d67d1caae74653bbc82d477e2d1f9f4a54040c
xleonhunter <xleonhunter@gmail.com>
2022-07-25 16:37:31 -0400

    updated method stubs

--- src/main/java/com/github/curriculeon/Playlist.java
+++ src/main/java/com/github/curriculeon/Playlist.java
@@ -4,78 +4,39 @@ package com.github.curriculeon;
  * Created by leon on 1/10/2021.
  */
 public class Playlist {
-    private int currentIndex;
-    private String[] songNameArray;
-
     public Playlist(String[] songNameArray) {
-        this.songNameArray = songNameArray;
     }
 
     public Playlist(int currentIndex, String[] songNameArray) {
-        this.currentIndex = currentIndex;
-        this.songNameArray = songNameArray;
     }
 
     public String[] getSongNameArray() {
-        return songNameArray;
+        return null;
     }
 
     public String getCurrentSelection() {
-        return this.songNameArray[currentIndex];
+        return null;
     }
 
     public void goToPreviousSong() {
-        Integer numberOfElements = songNameArray.length;
-        Integer lastIndex = numberOfElements - 1;
-        if (this.currentIndex == 0) {
-            this.currentIndex = lastIndex;
-        } else {
-            this.currentIndex--;
-        }
     }
 
     public void goToNextSong() {
-        Integer numberOfElements = songNameArray.length;
-        Integer lastIndex = numberOfElements - 1;
-        if (this.currentIndex >= lastIndex) {
-            this.currentIndex = 0;
-        } else {
-            this.currentIndex++;
-        }
     }
 
     public Integer getMinimumNumberOfStepsBetween(Integer currentIndex, String desiredSong) {
-        int forwardSteps = getForwardNumberOfStepsBetween(currentIndex, desiredSong);
-        int backwardSteps = getBackwardNumberOfStepsBetween(currentIndex, desiredSong);
-        if (forwardSteps > backwardSteps) {
-            return backwardSteps;
-        } else {
-            return forwardSteps;
-        }
+        return null;
     }
 
     public Integer getNumberOfStepsBetween(Integer currentIndex, String desiredSong, Runnable directionMutator) {
-        int numberOfSteps = 0;
-        this.currentIndex = currentIndex;
-        while (!desiredSong.equals(getCurrentSelection())) {
-            String songInPlaylist = songNameArray[currentIndex];
-            Boolean isSongInPlaylistCurrentSelectedSong = getCurrentSelection().equalsIgnoreCase(songInPlaylist);
-            Boolean isCurrentSelectedSongDesiredSong = getCurrentSelection().equalsIgnoreCase(desiredSong);
-            Boolean isCorrectSong = isSongInPlaylistCurrentSelectedSong && isCurrentSelectedSongDesiredSong;
-            if (isCorrectSong) {
-                return numberOfSteps;
-            }
-            directionMutator.run();
-            numberOfSteps++;
-        }
-        return numberOfSteps;
+        return null;
     }
 
     public Integer getForwardNumberOfStepsBetween(Integer currentIndex, String desiredSong) {
-        return getNumberOfStepsBetween(currentIndex, desiredSong, this::goToNextSong);
+        return null;
     }
 
     public Integer getBackwardNumberOfStepsBetween(Integer currentIndex, String desiredSong) {
-        return getNumberOfStepsBetween(currentIndex, desiredSong, this::goToPreviousSong);
+        return null;
     }
 }
