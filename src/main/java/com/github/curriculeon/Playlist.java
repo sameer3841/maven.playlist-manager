bcff1b189309dcc29a0d43ba52e57798a5a3b8ea
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2023-03-10 22:22:55 -0500

    got goToPreviousSong() and getMinimumNumberOfStepsBetween() to work, test cases pass :)

--- src/main/java/com/github/curriculeon/Playlist.java
+++ src/main/java/com/github/curriculeon/Playlist.java
@@ -4,28 +4,42 @@ package com.github.curriculeon;
  * Created by leon on 1/10/2021.
  */
 public class Playlist {
+
+    private int currentSong;
+    private String[] playlist;
+
     public Playlist(String[] songNameArray) {
+        playlist = songNameArray;
     }
 
     public Playlist(int currentIndex, String[] songNameArray) {
+        currentSong = currentIndex;
+        playlist = songNameArray;
     }
 
     public String[] getSongNameArray() {
-        return null;
+        return playlist;
     }
 
     public String getCurrentSelection() {
-        return null;
+        return getSongNameArray()[currentSong];
     }
 
     public void goToPreviousSong() {
+        if(currentSong > 0) currentSong--;
+        else currentSong = playlist.length - 1;
     }
 
     public void goToNextSong() {
+        if(currentSong < playlist.length - 1) currentSong++;
+        else currentSong = 0;
     }
 
     public Integer getMinimumNumberOfStepsBetween(Integer currentIndex, String desiredSong) {
-        return null;
+        currentSong = currentIndex;
+        while(!getCurrentSelection().equals(desiredSong))
+            goToNextSong();
+        return Math.abs(currentIndex - currentSong);
     }
 
     public Integer getNumberOfStepsBetween(Integer currentIndex, String desiredSong, Runnable directionMutator) {
